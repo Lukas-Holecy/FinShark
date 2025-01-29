469bdd450769762fb13180fc584774555e3c1f53
Lukas Holecy <lukas@brochet.co.uk>
2025-01-29 20:22:37 +0100

    Correctly initializing swagger on startup

--- api/Program.cs
+++ api/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
