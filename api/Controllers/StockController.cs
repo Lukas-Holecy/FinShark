496715c06b693f3e2084d29756e5aff7a4867a20
Lukas Holecy <lukas@brochet.co.uk>
2025-01-30 07:53:57 +0100

    Added update and delete endpoint for stocks and all endpoints are async

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -2,11 +2,13 @@ namespace api.Controllers;
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
 using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [Route("api/stock")]
 public class StockController(ApplicationDBContext context) : ControllerBase
@@ -14,31 +16,65 @@ public class StockController(ApplicationDBContext context) : ControllerBase
     private readonly ApplicationDBContext context = context;
 
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAllAsync()
     {
-        var stocks = context.Stocks.ToList().
-            Select(x => x.ToStockDto());
+        var stocks = await context.Stocks.ToListAsync();
+        var stockDtos = stocks.Select(x => x.ToStockDto());
         return Ok(stocks);
     }
 
     [HttpGet("{id}")]
-    public IActionResult GetById([FromRoute] int id)
+    public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
     {
-        var stock = context.Stocks.Find(id)?.ToStockDto();
+        var stock = await context.Stocks.FindAsync(id);
         if (stock == null)
         {
             return NotFound();
         }
 
-        return Ok(stock);
+        return Ok(stock.ToStockDto());
     }
 
     [HttpPost]
-    public IActionResult Create([FromBody] CreateStockDto stockDto)
+    public async Task<IActionResult> CreateAsync([FromBody] CreateStockDto stockDto)
     {
         var stockModel = stockDto.ToStock();
         context.Stocks.Add(stockModel);
-        context.SaveChanges();
-        return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+        await context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetByIdAsync), new { id = stockModel.Id }, stockModel.ToStockDto());
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync([FromRoute] int id, [FromBody] UpdateStockDto stockDto)
+    {
+        var stock = await context.Stocks.FindAsync(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        stock.Symbol = stockDto.Symbol;
+        stock.CompanyName = stockDto.CompanyName;
+        stock.Purchase = stockDto.Purchase;
+        stock.LastDiv = stockDto.LastDiv;
+        stock.Industry = stockDto.Industry;
+        stock.MarketCap = stockDto.MarketCap;
+
+        await context.SaveChangesAsync();
+        return Ok(stock.ToStockDto());
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAsync([FromRoute] int id)
+    {
+        var stock = await context.Stocks.FindAsync(id);
+        if (stock == null)
+        {
+            return NotFound();
+        }
+
+        context.Stocks.Remove(stock);
+        await context.SaveChangesAsync();
+        return NoContent();
     }
 }
